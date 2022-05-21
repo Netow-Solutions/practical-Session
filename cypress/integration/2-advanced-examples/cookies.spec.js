d679bf7fea9e634c8cc55cd7ff9301ced35ad1ad
RtrojanNuke <mubarak.a@menskin.com>
2022-05-21 11:07:33 +0100

    New test

--- cypress/integration/2-advanced-examples/cookies.spec.js
+++ cypress/integration/2-advanced-examples/cookies.spec.js
@@ -8,12 +8,12 @@ context('Cookies', () => {
 
     // clear cookies again after visiting to remove
     // any 3rd party cookies picked up such as cloudflare
-    cy.clearCookies()
+    cy.clearCookies();
   })
 
   it('cy.getCookie() - get a browser cookie', () => {
     // https://on.cypress.io/getcookie
-    cy.get('#getCookie .set-a-cookie').click()
+    cy.get('#getCookie .set-a-cookie').click();
 
     // cy.getCookie() yields a cookie object
     cy.getCookie('token').should('have.property', 'value', '123ABC')
